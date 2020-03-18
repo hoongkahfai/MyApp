78b0b6c1cec0de87635c652cd8c7006c1df2e902
Hoong Kah Fai <hoongkf-wm17@student.tarc.edu.my>
2020-03-19 07:48:58 +0800

    qq

--- app/src/main/java/com/example/myapp/LoginActivity.java
+++ app/src/main/java/com/example/myapp/LoginActivity.java
@@ -137,7 +137,7 @@ public class LoginActivity extends AppCompatActivity
                                 Toast.makeText(LoginActivity.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
                                 loadingBar.dismiss();
 
-                                Intent intent = new Intent(LoginActivity.this,AdminAddNewProductActivity.class);
+                                Intent intent = new Intent(LoginActivity.this,AdminCategoryActivity.class);
                                 startActivity(intent);
                             }
                             else if(parentDbName.equals("Users"))
